f657d4d1e04cde1ae128e5b63603da7c658ea6b5
Steve G <RailRodder@sympatico.ca>
2023-01-05 09:04:49 -0500

    Base for ClubBase 2023

--- java/src/jmri/jmrix/loconet/LocoNetSystemConnectionMemo.java
+++ java/src/jmri/jmrix/loconet/LocoNetSystemConnectionMemo.java
@@ -225,7 +225,9 @@ public class LocoNetSystemConnectionMemo extends DefaultSystemConnectionMemo imp
 
         ClockControl cc = getClockControl();
 
-        InstanceManager.setDefault(ClockControl.class, cc);
+        if (cc != null) {
+            InstanceManager.setDefault(ClockControl.class, cc);
+        }
 
         getIdTagManager();
 
@@ -280,7 +282,11 @@ public class LocoNetSystemConnectionMemo extends DefaultSystemConnectionMemo imp
         if (getDisabled()) {
             return null;
         }
-        return (LnClockControl) classObjectMap.computeIfAbsent(ClockControl.class,(Class<?> c) -> new LnClockControl(this));
+        if (get(ClockControl.class) == null) {
+            return (LnClockControl) classObjectMap.computeIfAbsent(ClockControl.class,
+                    (Class<?> c) -> new LnClockControl(this));
+        }
+        return null;
     }
 
     public LnReporterManager getReporterManager() {
