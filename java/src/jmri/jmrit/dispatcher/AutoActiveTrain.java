0f6d710d09dc1808238f723ce863c037b1f68708
Steve G <RailRodder@sympatico.ca>
2023-02-03 13:17:31 -0500

    restore blank line and restore loconet consisting

--- java/src/jmri/jmrit/dispatcher/AutoActiveTrain.java
+++ java/src/jmri/jmrit/dispatcher/AutoActiveTrain.java
@@ -1779,6 +1779,7 @@ public class AutoActiveTrain implements ThrottleListener {
                 log.error("Waiting for train to stop crashed - stop tasks not executing.", e);
             }
         }
+
         private final int _delay = 91;
         private int _task = 0;
     }
