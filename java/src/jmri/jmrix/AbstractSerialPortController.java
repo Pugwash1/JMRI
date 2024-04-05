e779cbc9d5f272acb94323c2da8afd15cd46e055
Steve G <RailRodder@sympatico.ca>
2024-04-05 16:37:10 -0400

    Stop Train in remaining distance. Allow primative filea access

--- java/src/jmri/jmrix/AbstractSerialPortController.java
+++ java/src/jmri/jmrix/AbstractSerialPortController.java
@@ -2,6 +2,10 @@ package jmri.jmrix;
 
 import java.io.*;
 import java.util.Vector;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 
 import jmri.SystemConnectionMemo;
 
@@ -77,6 +81,10 @@ abstract public class AbstractSerialPortController extends AbstractPortControlle
      */
     @Override
     public void connect() throws java.io.IOException {
+        if (mPort.startsWith("pipe:")) {
+            // do nothing here its done when getting datastreams
+            return;
+        }
         openPort(mPort, "JMRI app");
     }
 
@@ -472,6 +480,15 @@ abstract public class AbstractSerialPortController extends AbstractPortControlle
     // identical implementations in the subclasses - but note simulators are now overriding
     @Override
     public DataInputStream getInputStream() {
+        if (mPort.startsWith("pipe:")   ) {
+                try {
+                    return  new DataInputStream(new FileInputStream(mPort.substring(5)));
+                } catch (FileNotFoundException fnf) {
+                    log.error("getOutputStream exception: {}", fnf.getMessage());
+                }
+                return null;
+            }
+
         if (!opened) {
             log.error("getInputStream called before open, stream not available");
             return null;
@@ -483,6 +500,14 @@ abstract public class AbstractSerialPortController extends AbstractPortControlle
     // identical implementations in the subclasses - but note simulators are now overriding
     @Override
     public DataOutputStream getOutputStream() {
+        if (mPort.startsWith("pipe:")   ) {
+            try {
+                return  new DataOutputStream(new FileOutputStream(mPort.substring(5)));
+            } catch (FileNotFoundException fnf) {
+                log.error("getOutputStream exception: {}", fnf.getMessage());
+                return null;
+            }
+        }
         if (!opened) {
             log.error("getOutputStream called before open, stream not available");
         }
