975bf4ab29dd04cc818442921e9308cb3532bcbe
dabou <dabou@DBoudreau-2022>
2026-04-25 08:46:45 -0400

    Fix mistake

--- java/src/jmri/jmrit/operations/locations/tools/LocationCopyFrame.java
+++ java/src/jmri/jmrit/operations/locations/tools/LocationCopyFrame.java
@@ -116,7 +116,7 @@ public class LocationCopyFrame extends OperationsFrame implements java.beans.Pro
     }
 
     @Override
-    @SuppressFBWarnings(value = "ST_WRITE_TO_protected static_FROM_INSTANCE_METHOD", justification = "GUI ease of use")
+    @SuppressFBWarnings(value = "ST_WRITE_TO_STATIC_FROM_INSTANCE_METHOD", justification = "GUI ease of use")
     protected void buttonActionPerformed(java.awt.event.ActionEvent ae) {
         if (ae.getSource() == copyButton) {
             log.debug("copy location button activated");
